2d88daeda3d369f8c95e2b649592dc4ea6b6d503
kun.yang <1641250803@qq.com>
2025-01-02 16:08:30 +0800

    feat: 添加文件下载demo url

--- src/routes/fileUpload/index.js
+++ src/routes/fileUpload/index.js
@@ -1,8 +1,22 @@
 const app = require('express')
 const router = app.Router()
 
+const fs = require('node:fs')
+const path = require('node:path')
+
 router.get('/stream', function (req, res, next) {
-  res.send('stream')
+
+  const file = path.resolve(__filename, '../../../../public/test.zip')
+  const fileStream = fs.createReadStream(file);
+  const fileInfo = fs.statSync(file);
+
+  const head = {
+    'Content-Type': 'application/zip, application/octet-stream; charset=ISO-8859-1',
+    'Content-Disposition': 'attachment;filename=\"test.zip\"',
+    'Content-Length': fileInfo.size
+  };
+  res.writeHead(200, head);
+  fileStream.pipe(res);
 })
 
 
